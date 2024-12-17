9236b54e0d242c6960dd8e93d9a7790f5a62c512
josh-may <joshmay222@gmail.com>
2024-12-17 17:43:58 -0600

    Lazy commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-//
 
 export default function Home() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
